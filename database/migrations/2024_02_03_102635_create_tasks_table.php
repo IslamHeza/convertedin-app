f4ac8be41dc49a386d7cd0579515f100748c20e1
IslamHeza <islamo.heza@gmail.com>
2024-02-03 12:29:46 +0200

    create migrations for users,tasks and statistics tables

--- database/migrations/2024_02_03_102635_create_tasks_table.php
+++ database/migrations/2024_02_03_102635_create_tasks_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->integer('type');
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('assigned_to_id')->constrained('users')->onUpdate('cascade');
+            $table->foreignId('assigned_by_id')->constrained('users')->onUpdate('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
